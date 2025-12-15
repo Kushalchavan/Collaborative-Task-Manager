fe436513253892c74705ed7a81b30ac68ff963e1
Kushalchavan <kushalchavan2002@gmail.com>
2025-12-15 22:47:25 +0530

    chore(init): setup express and typescript

---
+++ server/src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.get("/health", (req , res) => {
+    res.json({status: "ok"});
+})
+
+export default app;
