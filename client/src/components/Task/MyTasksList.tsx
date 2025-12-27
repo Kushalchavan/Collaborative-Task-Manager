32aad1a9190528c5c3fd477b04ebcb4df5856eeb
Kushalchavan <kushalchavan2002@gmail.com>
2025-12-27 23:17:02 +0530

    feat(filtering): add fitering feature

--- client/src/components/Task/MyTasksList.tsx
+++ client/src/components/Task/MyTasksList.tsx
@@ -1,47 +1,60 @@
 import { Checkbox } from "@/components/ui/checkbox";
 import { Badge } from "@/components/ui/badge";
 import { Calendar } from "lucide-react";
-import { useGetTask } from "@/hooks/useTask";
 import { format } from "date-fns";
-import UpdateTaskModal from "./modals/UpdateTaskModal";
 import { useState } from "react";
+import { Spinner } from "../ui/spinner";
+import UpdateTaskModal from "../Dashboard/modals/UpdateTaskModal";
 
-type Task = {
+// ---------- Types ----------
+export type Task = {
   id: string;
   title: string;
-  descripion?: string;
+  description?: string;
   status: "todo" | "in-progress" | "done";
   priority: "high" | "medium" | "low";
   dueDate?: string;
   project?: string;
 };
 
-export default function TaskList() {
-  const { data } = useGetTask();
+type TaskListProps = {
+  tasks: Task[];
+  loading?: boolean;
+};
 
-  if (!data || data.data.length === 0) {
+// ---------- Task List ----------
+export default function MyTasksList({ tasks, loading }: TaskListProps) {
+  if (loading) {
+    return (
+      <div className="p-4 flex justity-center items-center">
+        <Spinner className="size-4" />
+      </div>
+    );
+  }
+
+  if (!tasks.length) {
     return (
       <div className="p-4 text-center text-sm text-muted-foreground">
-        No Tasks yet
+        No tasks found
       </div>
     );
   }
 
   return (
     <div className="rounded-xl border bg-background">
-      {data.data.map((task: Task) => (
+      {tasks.map((task) => (
         <TaskRow key={task.id} task={task} />
       ))}
     </div>
   );
 }
 
+// ---------- Task Row ----------
 function TaskRow({ task }: { task: Task }) {
-  const [completed, setCompleted] = useState<boolean>(false);
+  const [completed, setCompleted] = useState(task.status === "done");
+
   return (
-    <div
-      className={`flex items-center justify-between px-4 py-3 border-b last:border-none transition`}
-    >
+    <div className="flex items-center justify-between px-4 py-3 border-b last:border-none transition">
       {/* Left */}
       <div className="flex items-start gap-3">
         <Checkbox
@@ -52,14 +65,16 @@ function TaskRow({ task }: { task: Task }) {
 
         <div>
           <p
-            className={`text-sm font-medium ${
+            className={`text-sm font-medium transition ${
               completed ? "line-through text-muted-foreground" : ""
             }`}
           >
             {task.title}
           </p>
 
-          <p className="text-xs text-muted-foreground">{task.project}</p>
+          {task.project && (
+            <p className="text-xs text-muted-foreground">{task.project}</p>
+          )}
         </div>
       </div>
 
@@ -72,25 +87,13 @@ function TaskRow({ task }: { task: Task }) {
           {task.dueDate ? format(new Date(task.dueDate), "PP") : "No due date"}
         </div>
 
-        {/* <div className="flex -space-x-2">
-          <Avatar className="h-6 w-6 border">
-            <AvatarImage src="https://i.pravatar.cc/40" />
-            <AvatarFallback>U</AvatarFallback>
-          </Avatar>
-          <Avatar className="h-6 w-6 border">
-            <AvatarImage src="https://i.pravatar.cc/41" />
-            <AvatarFallback>U</AvatarFallback>
-          </Avatar>
-        </div> */}
-
-        <div>
-          <UpdateTaskModal task={task} />
-        </div>
+        <UpdateTaskModal task={task} />
       </div>
     </div>
   );
 }
 
+// ---------- Priority Badge ----------
 function PriorityBadge({ priority }: { priority: "high" | "medium" | "low" }) {
   const styles = {
     high: "bg-red-100 text-red-600",
@@ -99,12 +102,7 @@ function PriorityBadge({ priority }: { priority: "high" | "medium" | "low" }) {
   };
 
   return (
-    <Badge
-      variant="secondary"
-      className={
-        styles[priority.toLocaleLowerCase() as "high" | "medium" | "low"]
-      }
-    >
+    <Badge variant="secondary" className={styles[priority]}>
       {priority.charAt(0).toUpperCase() + priority.slice(1)}
     </Badge>
   );
--- client/src/pages/MyTasks.tsx
+++ client/src/pages/MyTasks.tsx
@@ -1,12 +1,66 @@
+import CreateTaskModal from "@/components/Dashboard/modals/CreateTaskModal";
 import ActivityPanel from "@/components/Task/ActivityPanel";
-import TasksFilters from "@/components/Task/TaskFilters";
-import TasksGroup from "@/components/Task/TasksGrounp";
+import MyTasksList from "@/components/Task/MyTasksList";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useGetTask } from "@/hooks/useTask";
 import DashboardLayout from "@/layout/DashboardLayout";
+import { useMemo, useState } from "react";
+
+type StatusFilter = "todo" | "in-progress" | "done" | "all";
+type PriorityFilter = "low" | "medium" | "high" | "all";
+type SortOrder = "dueDate-asc" | "dueDate-desc";
 
 const MyTasks = () => {
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState<StatusFilter>("all");
+  const [priority, setPriority] = useState<PriorityFilter>("all");
+  const [sort, setSort] = useState<SortOrder>("dueDate-asc");
+  const { data, isLoading } = useGetTask();
+  const tasks = data?.data ?? [];
+
+  const filteredTask = useMemo(() => {
+    let result = [...tasks];
+
+    // Search filtering
+    if (search) {
+      result = result.filter((task) =>
+        task.filter.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    // Status Filtering
+    if (status !== "all") {
+      result = result.filter((task) => task.status === status);
+    }
+
+    // Priority filter
+    if (priority !== "all") {
+      result = result.filter((task) => task.priority === priority);
+    }
+
+    // sorting due date
+    result.sort((a, b) => {
+      if (!a.dueDate) return 1;
+      if (!b.dueDate) return -1;
+      const aTime = new Date(a.dueDate).getTime();
+      const bTime = new Date(b.dueDate).getTime();
+
+      return sort === "dueDate-asc" ? aTime - bTime : bTime - aTime;
+    });
+
+    return result;
+  }, [tasks, search, status, priority, sort]);
+
   return (
     <DashboardLayout>
       <div className="flex gap-6">
@@ -29,16 +83,70 @@ const MyTasks = () => {
 
             <div className="flex items-center gap-3">
               <Badge variant="outline" className="gap-1">
-                <span className="size-3 bg-green-400 rounded-full mr-1 animate-pulse"/> Live updates
+                <span className="size-3 bg-green-400 rounded-full mr-1 animate-pulse" />{" "}
+                Live updates
               </Badge>
-              <Button>+ Add Task</Button>
+              <CreateTaskModal />
             </div>
           </div>
 
-          <TasksFilters />
+          {/* Task filtering */}
+          <div className="flex items-center justify-between gap-4">
+            <div className="flex gap-3">
+              {/* Searching */}
+              <Input
+                placeholder="Filter by name..."
+                className="w-64"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
 
-          <TasksGroup title="Today" />
-          <TasksGroup title="Upcoming" />
+              {/* Status */}
+              <Select
+                value={status}
+                onValueChange={(val) => setStatus(val as StatusFilter)}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Status" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="all">All</SelectItem>
+                  <SelectItem value="todo">Todo</SelectItem>
+                  <SelectItem value="in-progress">In Progress</SelectItem>
+                  <SelectItem value="done">Done</SelectItem>
+                </SelectContent>
+              </Select>
+
+              {/* Priority */}
+              <Select
+                value={priority}
+                onValueChange={(val) => setPriority(val as PriorityFilter)}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Priority" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="low">Low</SelectItem>
+                  <SelectItem value="medium">Medium</SelectItem>
+                  <SelectItem value="high">High</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <Button
+              variant="outline"
+              onClick={() =>
+                setSort((prev) =>
+                  prev === "dueDate-asc" ? "dueDate-desc" : "dueDate-asc"
+                )
+              }
+            >
+              Due date {sort === "dueDate-asc" ? "↑" : "↓"}
+            </Button>
+          </div>
+
+          {/* Task list */}
+          <MyTasksList tasks={filteredTask} loading={isLoading} />
         </div>
 
         {/* Right */}
