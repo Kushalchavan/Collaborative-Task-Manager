e74d5e52e3d93fa90a0db1e0ed4d31fb498bdfbd
Kushalchavan <kushalchavan2002@gmail.com>
2025-12-31 14:59:46 +0530

    fix(task) : include assigned tasksin user task list

--- server/src/repositories/task.repository.ts
+++ server/src/repositories/task.repository.ts
@@ -1,28 +1,33 @@
 import { Prisma } from "../../generated/prisma/client";
 import { prisma } from "../config/prisma";
 
+// Insert task
 export const insertTask = (data: Prisma.TaskCreateInput) => {
   return prisma.task.create({ data });
 };
 
+// Find task
 export const findTaskByIdFromDb = (id: string) => {
   return prisma.task.findUnique({
     where: { id },
   });
 };
 
+// Update TAsk
 export const updateTaskById = (id: string, data: Prisma.TaskUpdateInput) => {
   return prisma.task.update({ where: { id }, data });
 };
 
+// Delete Task
 export const deleteTaskById = (id: string) => {
   return prisma.task.delete({ where: { id } });
 };
 
+// Find task with user id
 export const findTaskByUserId = (userId: string) => {
   return prisma.task.findMany({
     where: {
-      createdById: userId,
+      OR: [{ createdById: userId }, { assignedToId: userId }],
     },
     orderBy: {
       createdAt: "desc",
